7f5eb09d4d06c619b0ce7cb6ed9e12c333592906
Caio <caiobarreto1acim@gmail.com>
2024-09-09 19:17:53 -0300

    config de deploys

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.superfrete.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: "/ReArts",
 });
